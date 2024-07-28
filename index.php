207ac924ab87441072646e92a959a584d79f6891
jeremyHarrell <hillvulf@gmail.com>
2024-07-28 15:25:44 -0500

    scaffold simple php calculator

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ; ?>" method="get">
+    <input type="number" name="num01" 
+    placeholder="number one">  
+    <select name="operator">
+      <option value="add">+</option>
+      <option value="subtract">-</option>
+      <option value="multiply">*</option>
+      <option value="divide">/</option>
+    </select>
+    <input type="number" name="num02" 
+    placeholder="number two">  
+  </form>
+
+</body>
+</html>
